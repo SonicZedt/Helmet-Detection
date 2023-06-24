63ac3c43d13e19fae17ded7e141e7bc223a82083
soniczedt <zulfikarhidayat1@gmail.com>
2023-06-24 10:39:29 +0700

    Removed config libraries and map

--- script/config.py
+++ script/config.py
@@ -4,12 +4,6 @@ config = json.load(open('config.json'))
 
 conf = config['conf']
 
-class Library:
-    tesseract_path = config['libraries']['tesseract.path']
-
-class Map:
-    location = config['map']['location']
-
 class Resolution:
     h = config['resolution']['height']
     w = config['resolution']['width']
